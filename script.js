f56bc9493e48bd5b861e5486e14bb567459fa3a3
JaredBona <107252982+JaredBona@users.noreply.github.com>
2023-03-28 23:35:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,159 @@
+
+function logIn() {
+	var name = prompt("Please enter your name: ", "nameless");
+  
+	if (name != null && name != "") {
+		var outputElement = document.getElementById("Greetings");
+  		outputElement.innerHTML = "Hello, " + name + "!";
+	}
+  } 
+
+function toggleImage1() {
+	var image = document.getElementById("image");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage2() {
+	var image = document.getElementById("image2");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage3() {
+	var image = document.getElementById("image3");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage4() {
+	var image = document.getElementById("image4");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage5() {
+	var image = document.getElementById("image5");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage6() {
+	var image = document.getElementById("image6");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage7() {
+	var image = document.getElementById("image7");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage8() {
+	var image = document.getElementById("image8");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage9() {
+	var image = document.getElementById("image9");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage10() {
+	var image = document.getElementById("image10");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage11() {
+	var image = document.getElementById("image11");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function show_alert() {
+  alert("WARNING! WARNING!");
+  }
+
+  function show_prompt()
+  {
+	var name=prompt("Please enter your name: ", "nameless");
+
+	  if (name!=null && name!="")
+	  {
+		document.write("Welcome " + name + "! Please come in");
+		document.write('<br/><br/><a href="Chapt8.html"><button>Go Back</button></a>');
+	  }
+  }
+
+  function goBack() {
+	  window.history.back();
+  }
+  
+function displaymessage()
+  {
+  alert("Hi There!");
+  }
+
+function toggleImage15() {
+	var image = document.getElementById("image15");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage16() {
+	var image = document.getElementById("image16");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
+
+function toggleImage17() {
+	var image = document.getElementById("image17");
+	if (image.style.display === "none") {
+	  image.style.display = "block";
+	} else {
+	  image.style.display = "none";
+	}
+  }
